2c4010c3333abe4c1ff98c09fb3c26cd6cc7da93
Mark de Wever <koraq@xs4all.nl>
2010-03-04 20:17:52 +0000

    wescamp_client: automatic update of addon 'TCG'

--- TCG/TCG.lua
+++ TCG/TCG.lua
@@ -1,10 +1,13 @@
 --TCG.lua - Provides tags for use in TCG eras
 
 H = wesnoth.require "lua/helper.lua"
-W = H.set_wml_action_metatable{} --Treats tags as callable functions.
-_ = wesnoth.textdomain "tcg"
 
+--WML constructs
+W = H.set_wml_action_metatable{} --Treats tags as callable functions.
+T = H.set_wml_tag_metatable{} --For subtags
 V = H.set_wml_var_metatable{} --Contains WML variables.
+
+_ = wesnoth.textdomain "tcg"
 
 local unitTypeList = {
    'Vampire Bat',
@@ -210,18 +213,19 @@ local unitTypeList = {
    'Fire Dragon',
 }
 
-local RecruitList = {}
-
 --This is a proxy for Wesnoth's recruit list that has a fixed length and allows
 --duplicates. Every time it changes, it will be copied to the current side's
---real recruit list and to WML variables. If this is a savegame, new() will
---restore data from WML variables. Otherwise, all entries will be nil.
+--real recruit list and to WML variables.
+local RecruitList = {}
+
+--If this is a savegame, restores data from WML variables. Otherwise, all
+--entries will be nil.
 function RecruitList:new()
    local list = {}
    setmetatable(list, self)
    self.__index = self
    local side = V.side_number
-   for i = 1, recruitListSize do
+   for i = 1, V.recruitListSize do
       list[i] = V['recruitList' .. side .. '_' .. i]
    end
    return list
@@ -231,21 +235,27 @@ end
 function RecruitList:set()
    local side = V.side_number
    local recruit = ''
-   for i = 1, recruitListSize do
+   for i = 1, V.recruitListSize do
       local type = self[i]
       V['recruitList' .. side .. '_' .. i] = type
       if type then
 	 recruit = recruit .. type .. ','
       end
    end
-   W.set_recruit{side = side, recruit = recruit}
+   W.set_recruit{
+      side = side,
+      recruit = recruit
+   }
 end
 
 --Fills all empty slots with randomly selected types from unitTypeList.
 function RecruitList:fill()
-   for i = 1, recruitListSize do
+   for i = 1, V.recruitListSize do
       if not self[i] then
-	 W.set_variable{name = 'typeN', rand = '1..' .. #unitTypeList}
+	 W.set_variable{
+	    name = 'typeN',
+	    rand = '1..' .. #unitTypeList
+	 }
 	 self[i] = unitTypeList[V.typeN]
       end
    end
@@ -256,11 +266,11 @@ end
 --Removes one instance of V.unit.type from self.
 --i: Used only in recursion.
 function RecruitList:remove(i)
-   i = i or recruitListSize
+   i = i or V.recruitListSize
    local type = V.unit.type
    if i < 1 then
       wesnoth.message(
-	 'TCG: RecruitList:remove: ' .. type .. ' not on recruit list')
+	 'TCG: RecruitList:remove: ' .. type .. _' not on recruit list')
    elseif self[i] == type then
       self[i] = nil
       self:set()
@@ -271,11 +281,59 @@ end
 
 function RecruitList:__tostring()
    local result = ''
-   for i = 1, recruitListSize do
+   for i = 1, V.recruitListSize do
       local type = self[i] or ''
       result = result .. i .. ': ' .. type .. "\n"
    end
    return result
+end
+
+--Returns an array of hexes.
+--wml: WML table to send to the [store_locations] tag. (The variable is
+--automatically provided and will be cleared after use.)
+local function getLocations(wml)
+   wml.variable = 'hexes'
+   W.store_locations(wml)
+   local hexes = H.get_variable_array(wml.variable)
+   V[wml.variable] = nil
+   return hexes
+end
+
+--Used for scanning the map. Stores its own dimensions and a boolean for each
+--hex which is true if it has been checked.
+local Map = {}
+
+function Map:new()
+   local map = {}
+   setmetatable(map, self)
+   self.__index = self
+   map.xMax, map.yMax = wesnoth.get_map_size()
+   for i = 1, map.xMax do
+      map[i] = {}
+   end
+   return map
+end
+
+--Counts the tiles in the castle that hex is in.
+--hex: The tile to start counting from.
+function Map:countCastle(hex)
+   local x = hex.x
+   local y = hex.y
+   if self[x][y] or not wesnoth.get_terrain_info(hex.terrain).castle then
+      return 0
+   else
+      self[x][y] = true
+      local hexes = getLocations{
+	 x = x,
+	 y = y,
+	 radius = 1
+      }
+      local result = 1
+      for i, h in ipairs(hexes) do
+	 result = result + self:countCastle(h)
+      end
+      return result
+   end
 end
 
 local recruitLists = {}
@@ -294,13 +352,26 @@ local function removeType()
    recruitLists[V.side_number]:remove()
 end
 
+--Returns the recruitment capacity of the largest castle.
+local function biggestCastle()
+   keeps = getLocations{
+      terrain = 'K*'
+   }
+   local map = Map:new()
+   local result = 0
+   for i, hex in ipairs(keeps) do
+      result = math.max(result, map:countCastle(hex) - 1)
+   end
+   return result
+end
+
 --Sets the maximum size for recruit lists. Will not be preserved in savegames
 --unless used in a preload event.
---n: Number of slots.
+--n: Number of slots. Default is the biggest castle size on the map.
 wesnoth.register_wml_action(
    'set_recruit_list_size',
    function(cfg)
-      recruitListSize = cfg.n
+      V.recruitListSize = cfg.n or biggestCastle()
    end)
 
 --Fills the empty slots in the current side's recruit list with random unit
@@ -324,12 +395,30 @@ wesnoth.register_wml_action(
       recruitLists[V.side_number]:fill()
    end)
 
---Shows a message telling the player exactly what is in their Lua-defined
---recruit list. Useful for finding out which, if any, unit type has been
+local idShowRecruit = 'show_recruit'
+
+--Shows recruit list to the player.
+wesnoth.register_wml_action(
+   idShowRecruit,
+   function()
+      local side = V.side_number
+      W.message{
+	 side_for = side,
+	 message = tostring(recruitLists[side])
+      }
+   end)
+
+--Sets a menu item that tells the player exactly what is in their Lua-defined
+--recruit list. Useful for finding out which unit type, if any, has been
 --duplicated.
 wesnoth.register_wml_action(
-   'show_recruit_list',
+   'menu_' .. idShowRecruit,
    function()
-      side = V.side_number
-      W.message{side_for = side, message = tostring(recruitLists[side])}
+      W.set_menu_item{
+	 id = idShowRecruit,
+	 description = _'Show actual recruit list',
+	 T.command{
+	    T[idShowRecruit]{}
+	 }
+      }
    end)
